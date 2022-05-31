3e398953cbdc7021773f6a6d2a21bc431a5e5879
Obada Tumah <obadatumah19@gmail.com>
2022-05-31 16:38:53 +0300

    adding table to the students component

--- src/components/students/students.js
+++ src/components/students/students.js
@@ -1,17 +1,183 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import TablePagination from '@mui/material/TablePagination';
+import Header from '../header/header';
+import { Footer } from '../footer/footer';
 
-import Header from "../header/header";
-import { Footer } from "../footer/footer";
+function createData(name, calories, fat, carbs, protein, price) {
+    return {
+        name,
+        calories,
+        fat,
+        carbs,
+        protein,
+        price,
+        history: [
+            {
+                date: '2020-01-05',
+                email: 'email@domain.com',
+                phone: '00962781578711',
+                major: 'CS',
+                english: "Very Good"
+            },
+        ],
+    };
+}
 
-
-function Students() {
+function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
 
     return (
-        <>
-        <Header />
-        <p>This is students page</p>
-        <Footer />
-        </>
+        <React.Fragment>
+            <TableRow >
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Details
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Joining Date</TableCell>
+                                        <TableCell>Email</TableCell>
+                                        <TableCell >Phone Number</TableCell>
+                                        <TableCell >University major</TableCell>
+                                        <TableCell >English Proficiency </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {row.history.map((historyRow) => (
+                                        <TableRow key={historyRow.date}>
+                                            <TableCell component="th" scope="row">
+                                                {historyRow.date}
+                                            </TableCell>
+                                            <TableCell>{historyRow.email}</TableCell>
+                                            <TableCell >{historyRow.phone}</TableCell>
+                                            <TableCell >{historyRow.major}</TableCell>
+                                            <TableCell >{historyRow.english}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
     );
 }
 
-export default Students;
+Row.propTypes = {
+    row: PropTypes.shape({
+        calories: PropTypes.number.isRequired,
+        carbs: PropTypes.number.isRequired,
+        fat: PropTypes.number.isRequired,
+        history: PropTypes.arrayOf(
+            PropTypes.shape({
+                amount: PropTypes.number.isRequired,
+                customerId: PropTypes.string.isRequired,
+                date: PropTypes.string.isRequired,
+            }),
+        ).isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        protein: PropTypes.number.isRequired,
+    }).isRequired,
+};
+
+const rows = [
+    createData('Ahmad Ali', "Python", 4, "Graduated", '2020-01-05', 1),
+    createData('Anas Khalid', "JS", 6, "Student", '2022-01-05', 4.99),
+    createData('Asmaa Raed', ".Net", 2, "Student", '2022-01-05', 3.79),
+    createData('Ashraf Saif', "Java", 7, "Suspended", '2022-01-05', 2.5),
+    createData('Amani Rami', "JS", 5, "Employeed", '2021-01-05', 1.5),
+    createData('Ehsan Malik', "Cyber Security", 1, "Student", '2022-01-05', 2),
+];
+
+export default function Students() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    return (
+        <>
+            <Header></Header>
+            <TableContainer component={Paper} >
+                <Table aria-label="collapsible table" >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Course</TableCell>
+                            <TableCell align="right">Course Number</TableCell>
+                            <TableCell align="right">Status</TableCell>
+                            <TableCell align="right">Finishing Date</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                            <Row key={row.name} row={row} />
+                        ))}
+
+                        
+                    </TableBody>
+
+                </Table>
+                <TablePagination
+                            rowsPerPageOptions={[5, 10, 25]}
+                            component="div"
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+            </TableContainer>
+            <Footer></Footer>
+        </>
+    );
+}
